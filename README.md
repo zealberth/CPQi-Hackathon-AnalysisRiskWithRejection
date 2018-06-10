22d8dd1de37081df43f2198c819af19d59c34e6f
AtillaMaia <atillanegreirosmaia@gmail.com>
2018-06-10 17:53:52 -0300

    Improvements

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # CPQi-Hackathon-AnalysisRiskWithRejection
+
+## TEAM: Atilla Maia, Francisco Felipe, Jose Alberth
+
+Our problem is related to risk analysis, where we have to classify whether or not a customer will pay a loan based on
+
+past payments and personal attributes (profile).
+
+The dataset used in this hackathon is available in UCI Machine Learning (https://archive.ics.uci.edu/ml/datasets/default+of+credit+card+clients).
+
+About the dataset, it is unbalanced, approximately 22% of the samples belongs to one class (0 - non-risky). Therefore, another dataset was generated
+
+(through the reduction of the class with more amount of patterns), in order to balance (generalize) the data. Experiments were performed using both datasets, 20 executions for each dataset .
+
+The classification model is based on an Ensemble learning. The model consists of the following methods: Support Vector Machine (SVM), MultiLayer Perceptron (MLP), Naive Bayes (NB), Gaussian Mixture Models (GMM),
+
+and k-Nearest Neighborhood (KNN) classifiers. The vote of each classifier has weight 1. Also, we developed a Reject Option method with the Ensemble learning.
+
+Thus, the ensemble model will only classify a sample when at least four classifiers agree (at least 80% of classifiers). When the ensemble diverges (3 or 2 against), the sample is rejected, which means it is
+
+selected to be classified later  by an expert in the area.
+
+The training phase of each model is applied Grid Search with k-fold cross-validation (k = 5) in order to find the best parameters.
+
+The script 'run.py' execute the Ensemble model.  
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import  numpy as np
+import matplotlib.pyplot as plt
 
 from ensemble import Ensemble
 from sklearn import preprocessing
@@ -8,41 +9,55 @@ from sklearn.model_selection import train_test_split
 
 def main():
     # Dataset path
-    dataset_path = os.getcwd() + "\\dataset\\credit_card_clients_balanced.csv"
-    dataset = pd.read_csv(dataset_path, encoding='utf-8')
+    dataset_name = ['credit_card_clients_balanced',
+                    'credit_card_clients']
 
-    # Datasets columns
-    data_x = dataset[
-        ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11',
-         'X12', 'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21',
-         'X22', 'X23']]
-    data_y = dataset['Y']
+    for data_name in dataset_name:
+        dataset_path = os.getcwd() + "\\dataset\\" + data_name + ".csv"
+        dataset = pd.read_csv(dataset_path, encoding='utf-8')
 
-    # Preprocessing data
-    min_max_scaler = preprocessing.MinMaxScaler()
-    X_normalized = min_max_scaler.fit_transform(data_x)
+        # Datasets columns
+        data_x = dataset[
+            ['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9', 'X10', 'X11',
+             'X12', 'X13', 'X14', 'X15', 'X16', 'X17', 'X18', 'X19', 'X20', 'X21',
+             'X22', 'X23']]
+        data_y = dataset['Y']
 
-    error_rate = []
-    reject_rate = []
-    # Runs to test the model
-    for i in range(5):
-        print('---------------- Ensemble -----------------')
-        print('--- MLP - SVM - KNN - GMM - Naive Bayes ---')
-        print(i + 1, 'of 20 iterations')
-        X_train, X_test, y_train, y_test = train_test_split(X_normalized, data_y,
-                                                        test_size=0.2)
-        y_train = np.array(y_train)
-        y_test = np.array(y_test)
+        # Preprocessing data
+        min_max_scaler = preprocessing.MinMaxScaler()
+        X_normalized = min_max_scaler.fit_transform(data_x)
 
-        model = Ensemble()
-        model.train(X_train, y_train, gridSearch=False)
-        y_hat = model.predict(X_test)
+        acc_rate = []
+        reject_rate = []
 
-        error, reject = model.evaluate(y_hat, y_test)
-        error_rate.append(error)
-        reject_rate.append(reject)
-    return  error_rate, reject_rate
+        # Runs to test the model
+        for i in range(20):
+            print('---------------- Ensemble -----------------')
+            print('--- MLP - SVM - KNN - GMM - Naive Bayes ---')
+            print(i + 1, 'of 20 iterations')
+            X_train, X_test, y_train, y_test = train_test_split(X_normalized, data_y,
+                                                            test_size=0.2)
+            y_train = np.array(y_train)
+            y_test = np.array(y_test)
+
+            model = Ensemble()
+            model.train(X_train, y_train, gridSearch=False)
+            y_hat = model.predict(X_test)
+
+            error, reject = model.evaluate(y_hat, y_test)
+            acc_rate.append(1 - error)
+            reject_rate.append(reject)
+
+        graphics(acc_rate, reject_rate, data_name)
+
+def graphics(acc_rate, reject_rate, data_name):
+    plt.plot(reject_rate, acc_rate, '*-')
+    plt.ylabel('Accuracy rate')
+    plt.xlabel('Rejection rate')
+    plt.suptitle('Accuracy x Rejection for each iteration')
+    plt.savefig(os.getcwd() + '\\graphics\\AccuracyVsRejection_' + data_name +
+                ".png")
 
 
 if __name__ == '__main__':
-    error_rate, reject_rate = main()
+    main()
